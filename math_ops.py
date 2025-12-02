1554d8cceb9886de486dca5c7919a28ebe1fc832
Hardik <hardikgavali123@gmail.com>
2025-12-02 15:13:41 +0530

    add math subtraction multiplication and division function

--- math_ops.py
+++ math_ops.py
@@ -1,2 +1,11 @@
 def add(a,b):
     return a+b
+def sub(a,b):
+    return a-b
+def multi(a,b):
+    return a*b
+def divide(a,b):
+    if b==0:
+        return"dividion not possible"
+    else:
+        return a/b
